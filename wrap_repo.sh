ada50158339b476209d04c421ce1f23d4aeeb547
kymmt80 <kimiya80@gmail.com>
2022-07-04 23:01:14 +0430

    refactor: move input-reading logic to function

--- wrap_repo.sh
+++ wrap_repo.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+get_input(){
+    touch tmp/repos_list.txt
+    echo "" >> ./tmp/repos_list.txt
+    echo "" >> ./tmp/repos_list.txt
+    echo "# PLEASE ENTER THE LINKS TO YOUR REPOSITORIES THAT YOU WISH TO WRAP (ONE IN EACH LINE):" >> ./tmp/repos_list.txt
+    echo "# https://github.com/Miookie/Repo-Wrapper.git " >> ./tmp/repos_list.txt
+    nano ./tmp/repos_list.txt
+}
 mkdir tmp
-touch tmp/repos_list.txt
-echo "" >> ./tmp/repos_list.txt
-echo "" >> ./tmp/repos_list.txt
-echo "# PLEASE ENTER THE LINKS TO YOUR REPOSITORIES THAT YOU WISH TO WRAP (ONE IN EACH LINE):" >> ./tmp/repos_list.txt
-echo "# https://github.com/Miookie/Repo-Wrapper.git " >> ./tmp/repos_list.txt
-nano ./tmp/repos_list.txt
+get_input
 
 rm -r tmp/
